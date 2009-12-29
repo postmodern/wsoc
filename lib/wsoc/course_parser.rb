731bd4e520a090f661020676d91129ec685b8e9c
postmodern <postmodern.mod3@gmail.com>
2009-12-29 12:28:13 -0800

    Added the CourseParser module.

---
+++ lib/wsoc/course_parser.rb
@@ -0,0 +1,65 @@
+#
+# WSOC - The Web Spider Obstacle Course
+#
+# Copyright (c) 2009 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'wsoc/extensions/uri'
+
+require 'nokogiri'
+
+module WSOC
+  module CourseParser
+    def link_to_spec(path,link)
+      relative_path = (link.get_attribute('href') || '')
+      absolute_path = URI.expand_path(File.join('',path,relative_path))
+
+      return {
+        :message => link.inner_text,
+        :link => relative_path,
+        :url => absolute_path,
+        :example => link.to_html
+      }
+    end
+
+    def parse_page(path)
+      doc = Nokogiri::HTML(open(page))
+
+      doc.search('.follow//a').each do |follow|
+        Specs << link_to_spec(path,follow).merge(:behavior => :follow)
+      end
+
+      doc.search('.nofollow//a').each do |nofollow|
+        Specs << link_to_spec(path,nofollow).merge(:behavior => :nofollow)
+      end
+
+      doc.search('.ignore//a').each do |ignore|
+        Specs << link_to_spec(path,ignore).merge(:behavior => :ignore)
+      end
+
+      doc.search('.fail//a').each do |failed|
+        Specs << link_to_spec(path,failed).merge(:behavior => :fail)
+      end
+
+      return specs
+    end
+
+    def parse_course
+      Dir[self.course,'**','*.html'].each { |page| parse_page(page) }
+    end
+  end
+end
