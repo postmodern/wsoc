a610f3e0ef2db905f8babc617956f7d3263404be
postmodern <postmodern.mod3@gmail.com>
2009-12-29 21:02:10 -0800

    Require 'wsoc/course_specs'.

--- lib/wsoc/app.rb
+++ lib/wsoc/app.rb
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'wsoc/specs'
+require 'wsoc/course_specs'
 require 'wsoc/helpers'
 require 'wsoc/version'
 
