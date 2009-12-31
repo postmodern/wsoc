28c0bdbd84b6bdbc13774a8d0131fe64f6e061c6
postmodern <postmodern.mod3@gmail.com>
2009-12-30 20:34:58 -0800

    Added code to WSOC::Runner.
    * Renamed bin/wsoc to bin/wsoc_server, to match the README.

--- lib/wsoc/runner.rb
+++ lib/wsoc/runner.rb
@@ -18,15 +18,78 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'wsoc/app'
+
 require 'optparse'
 
 module WSOC
   class Runner
 
+    # Default host to run the WSOC server on
+    DEFAULT_HOST = 'localhost'
+
+    # Default port to run the WSOC server on
+    DEFAULT_PORT = 8080
+
+    # Host to run the WSOC server on
+    attr_reader :host
+
+    # Port to run the WSOC server on
+    attr_reader :port
+
     def initialize
+      @host = DEFAULT_HOST
+      @port = DEFAULT_PORT
+      @handler = nil
     end
 
     def Runner.start(args=ARGV)
+      runner = self.new()
+      runner.run(*args)
+    end
+
+    def run(*args)
+      optparse(*args)
+
+      App.run!(
+        :env => :production,
+        :host => @host,
+        :port => @port,
+        :server => @handler
+      )
+    end
+
+    protected
+
+    def optparse(*args)
+      opts = OptionParser.new
+
+      opts.banner = "usage: #{$0} [options]"
+
+      opts.on('-H','--host HOST',"The host to run the server on","Default: #{DEFAULT_HOST}") do |host|
+        @host = host
+      end
+
+      opts.on('-p','--port PORT',"The port to run the server on","Default: #{DEFAULT_PORT}") do |port|
+        @port = port.to_i
+      end
+
+      opts.on('--s','--server NAME','Rack handler to run the server under') do |handler|
+        @handler = handler
+      end
+
+      opts.on('-h','--help','Display the help output') do
+        puts opts
+        exit
+      end
+
+      begin
+        opts.parse!(args)
+      rescue OptionParser::InvalidOption => e
+        STDERR.puts e.message
+        STDERR.puts opts
+        exit -1
+      end
     end
 
   end
