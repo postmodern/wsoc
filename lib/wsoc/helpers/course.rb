e64a244313a680a91e799619190d82535945d16e
postmodern <postmodern.mod3@gmail.com>
2009-12-30 18:09:07 -0800

    Added a specs helper method.

--- lib/wsoc/helpers/course.rb
+++ lib/wsoc/helpers/course.rb
@@ -18,6 +18,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'wsoc/course_specs'
+
 module WSOC
   module Helpers
     module Course
@@ -31,6 +33,10 @@ module WSOC
         s << path
 
         return s
+      end
+
+      def specs
+        CourseSpecs.specs_for(request.host,request.port)
       end
     end
   end
